45c75b3e524e33ef1410ee3023c5ebd74147571d
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-01 22:37:47 +0100

    fixed

--- email/mainemail.js
+++ email/mainemail.js
@@ -3,7 +3,7 @@ let userInput = document.getElementById("emailInput");
 document.getElementById("confirmButton").addEventListener("click", function(){
     const registeredMail = [ "pippi@gmail.com"];
     let newEmail = userInput.value;
-    if (registeredMail.includes(newEmail) === true){
+    if (registeredMail.includes(newEmail)){
         document.getElementById("message").innerHTML = "Benvenuto!";
     } else document.getElementById("message").innerHTML = "Non hai accesso.";
 
