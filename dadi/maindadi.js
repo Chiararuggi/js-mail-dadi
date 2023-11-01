75f96c12749bdbd2284ab016b55a7eb2572d7f95
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-01 23:42:35 +0100

    dadi

---
+++ dadi/maindadi.js
@@ -0,0 +1,18 @@
+let userInput = document.getElementById("userNumberInput");
+
+document.getElementById("generateNumber").addEventListener("click", function(){
+    let casualNumber = Math.floor( Math.random() * 7 );
+    let userNumber = userInput.value;
+
+    if(userNumber > 6){
+        document.getElementById("message").innerHTML = "Inserisci un numero valido";
+        return;
+    }
+
+    document.getElementById("winOrloseMessage").innerHTML = "Il numero è " + casualNumber;
+    if(userNumber > casualNumber){
+        
+        document.getElementById("message").innerHTML = "Hai vinto!";
+
+    } else document.getElementById("message").innerHTML = "Hai perso...";
+})
